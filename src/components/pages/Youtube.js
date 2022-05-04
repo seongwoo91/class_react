f582aacbac350b50a8cadf8212dad4f479c7af72
seongwoo91 <seongwoo4477@gmail.com>
2022-05-04 17:04:12 +0900

    사용되지않는 임폴트정리

--- src/components/pages/Youtube.js
+++ src/components/pages/Youtube.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import Header from "../layout/Header";
 import Contents from "../layout/Contents";
 import Footer from "../layout/Footer";
@@ -6,104 +6,91 @@ import Title from "../layout/Title";
 import Contact from "../layout/Contact";
 import YoutubeList from "../includes/YoutubeList";
 import YoutubeSearch from "../includes/YoutubeSearch";
-import react, {useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 import { gsap } from "gsap";
-import Loading from "../basics/Loading";
-
-
-
 
 function Youtube() {
+  const [video, setvideo] = useState([]);
 
-   
-    const [video, setvideo] = useState([]);
+  const mainAnimation = () => {
+    setTimeout(() => {
+      gsap.to("#header", {
+        duration: 0.8,
+        top: 0,
+      });
+      gsap.to("#footer", {
+        duration: 0.8,
+        bottom: 0,
+        delay: 0.2,
+      });
+      gsap.to(".cont__title strong", {
+        duration: 0.7,
+        y: 0,
+        opacity: 1,
+        delay: 1.0,
+        ease: "power4.out",
+      });
+      gsap.to(".cont__title em", {
+        duration: 0.7,
+        y: 0,
+        opacity: 1,
+        delay: 1.3,
+        ease: "power4.out",
+      });
+    }, 10);
+  };
 
+  const search = (query) => {
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
 
-   const mainAnimation = () => {
-        setTimeout(() => { 
-            gsap.to("#header", {
-                duration: 0.8, 
-                top: 0,
-            });
-            gsap.to("#footer", {
-                duration: 0.8, 
-                bottom: 0,
-                delay: 0.2,
-            });
-            gsap.to(".cont__title strong", {
-                duration: 0.7,
-                y: 0,
-                opacity: 1,
-                delay: 1.0,
-                ease: "power4.out"
-            });
-            gsap.to(".cont__title em", {
-                duration: 0.7,
-                y: 0,
-                opacity: 1,
-                delay: 1.3,
-                ease: "power4.out"
-            });
-            
-        }, 10)
-    }
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=30&q=${query}&key=${process.env.REACT_APP_API}&type=video`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setvideo(result.items))
+      .catch((error) => console.log("error", error));
+  };
 
+  useEffect(() => {
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
 
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=30&q=pianoman&key=${process.env.REACT_APP_API}&type=video`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        setvideo(result.items);
+        mainAnimation();
+      })
+      .catch((error) => console.log("error", error));
+  }, []);
 
-    const search = (query) => {
-        var requestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-          };
-          
-          fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=30&q=${query}&key=${process.env.REACT_APP_API}&type=video`, requestOptions)
-            .then(response => response.json())
-            .then(result => setvideo(result.items))
-            .catch(error => console.log('error', error));
-    }
-
-    useEffect(()=>{
-        var requestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-          };
-          
-          fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=30&q=pianoman&key=${process.env.REACT_APP_API}&type=video`, requestOptions)
-            .then(response => response.json())
-            .then(result => {
-                setvideo(result.items)
-                mainAnimation()
-                })
-            .catch(error => console.log('error', error));
-    }, []);
-
-  
-    
-
-    
-   
-    
-        return (        
-          
-                  <>
-                      <Header/>
-                      <Contents>
-                          <Title title={["youtube","reference"]} />
-                          <section className="youtube__cont">
-                              <div className="container">
-                                  <div className="youtube__inner">   
-                                  <YoutubeSearch onSearch={search} />
-                                  <YoutubeList videos={video}/>
-                                  </div>
-                              </div>
-                          </section>        
-                          <Contact />
-                      </Contents>
-                      <Footer/>
-                  </>
-         
-        )
-   
+  return (
+    <>
+      <Header />
+      <Contents>
+        <Title title={["youtube", "reference"]} />
+        <section className="youtube__cont">
+          <div className="container">
+            <div className="youtube__inner">
+              <YoutubeSearch onSearch={search} />
+              <YoutubeList videos={video} />
+            </div>
+          </div>
+        </section>
+        <Contact />
+      </Contents>
+      <Footer />
+    </>
+  );
 }
 
-export default Youtube
+export default Youtube;
